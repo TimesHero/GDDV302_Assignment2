04ac96dcd7a9efe51a853e4043f05eee4135ad2f
TimesHero <alexander.aj.fraser@gmail.com>
2025-11-09 16:42:08 -0500

    Walking/Running Forward Works
    But for some fucking reason it only works with the AnimationController we made in class. (Leonard) and not the ones I'm making fresh for this project.

--- Assets/Scripts/PlayerLocomotion.cs
+++ Assets/Scripts/PlayerLocomotion.cs
@@ -1,34 +1,25 @@
 using UnityEngine;
-using System;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerLocomotion : MonoBehaviour
 {
-    Animator anim; //talks to animator component, updates mecanim parameters (speed, etc)
-    CharacterController cc; //talks to the controller on the player root
+    public Animator animator;
+    private readonly int xAxisParam = Animator.StringToHash("xAxis");
+    private readonly int yAxisParam = Animator.StringToHash("yAxis");
+
 
     void Awake() //runs when GameObject activates
     {
-        anim = GetComponentInChildren<Animator>(); //searches children until Animator found
-        cc = GetComponent<CharacterController>(); //looks for controller attached to player
+        animator = GetComponent<Animator>(); //searches children until Animator found
     }
 
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal"); //self explanatory - left and right
-        float z = Input.GetAxisRaw("Vertical"); // ^That^ - forward and back
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        Vector2 joypadStick = new Vector2(x, z); //combines both inputs into one "direction"
-        float baseSpeed = Mathf.Clamp01(joypadStick.magnitude); //how hard the stick is being pushed
-        
-        float stickDeadZone = 0.15f; //just in case I've got some drift
-        if (baseSpeed < stickDeadZone)
-        {
-            baseSpeed = 0f; 
-        }
+        animator.SetFloat(xAxisParam, horizontalInput);
+        animator.SetFloat(yAxisParam, verticalInput);
 
-        bool isRunning = Input.GetKey(KeyCode.LeftShift); //shift to run
-        float targetSpeed = isRunning ? baseSpeed : MathF.Min(baseSpeed, 0.5f);
-
-        anim.SetFloat("Speed", targetSpeed); //lets animator know how fast to blend
     }
 }
